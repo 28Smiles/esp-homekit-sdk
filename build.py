60b64aa427da828c9166d0f16241fa65c30a6cda
Philipp Scheff <scheff@chargery.de>
2021-09-24 09:33:55 +0200

    add button.c patch

--- build.py
+++ build.py
@@ -9,6 +9,7 @@ examples_common = "examples/common"
 common= "components/common"
 
 process = subprocess.call(["git", "submodule", "update", "--init", "--recursive"],stdout=open(os.devnull, 'wb'))
+process = subprocess.call(["git", "apply", "patches/button_glitch_filter_time_ms.patch"],stdout=open(os.devnull, 'wb'))
 
 if not Path(common).exists():
     shutil.copytree(examples_common, common)
